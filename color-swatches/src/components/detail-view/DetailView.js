99e09dd2b33414ce45437dc7f3194722eb7eb54f
Lillian Gales <lilliangales@ymail.com>
2019-12-10 17:01:20 -0800

    random color and selected color detail view working

--- color-swatches/src/components/detail-view/DetailView.js
+++ color-swatches/src/components/detail-view/DetailView.js
@@ -5,15 +5,8 @@ import { tsPropertySignature } from '@babel/types';
 
 function DetailView(props) {
 
-  // const [selectedColor, setSelectedColor] = useState(props.location.state.comingFromSelected ? [props.location.state.colorSelected] : [selectRandomColor()]);
+  const selectedColor = props.location.state.comingFromSelected ? [props.location.state.colorSelected] : [selectRandomColor()];
 
-  const [selectedColor, setSelectedColor] = useState([selectRandomColor()]);
-
-  console.log('comingFromSelected', props.location.state.comingFromSelected,
-  props.location.state.comingFromSelected ? [props.location.state.colorSelected] : [selectRandomColor()])
-
-
-  // if coming from random
   function selectRandomColor() {
     const randomIndex = Math.floor(Math.random() * props.colors.length);
     return props.colors[randomIndex];
@@ -22,55 +15,9 @@ function DetailView(props) {
   return (
     <div>
       <p></p>
-      {/* <div style={{backgroundColor: }}></div> */}
       <ColorSwatch currentColors={selectedColor} colorSwatchWidth='400px' colorSwatchHeight='365px'/>
     </div>
   );
 }
-
-
-
-// class DetailView extends React.Component {
-
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       selectedColor: this.props.location.state.comingFromSelected ? [this.props.location.state.colorSelected] : [this.selectRandomColor()]
-//     }
-//     // const [selectedColor] = useState(this.props.location.state.comingFromSelected ? [this.props.location.state.colorSelected] : [this.selectRandomColor()]);
-//   }
-  
-
-//   selectRandomColor = () => {
-
-//     const randomIndex = Math.floor(Math.random() * this.props.colors.length);
-//     return this.props.colors[randomIndex];
-
-//   }
-
-//   render() {
-   
-
-//   // setSelectedColor(props.location.state.comingFromSelected ? [props.location.state.colorSelected] : [selectRandomColor()]);
-
-//     console.log('comingFromSelected', this.props.location.state.comingFromSelected,
-//     this.props.location.state.comingFromSelected ? [this.props.location.state.colorSelected] : [this.selectRandomColor()])
-
-
-//   // if coming from random
-    
-
-//   return (
-//     <div>
-//       <p></p>
-//       <ColorSwatch currentColors={this.state.selectedColor} colorSwatchWidth='400px' colorSwatchHeight='365px'/>
-//     </div>
-//   );
-
-
-
-//   }
-  
-// }
 
 export default DetailView;
