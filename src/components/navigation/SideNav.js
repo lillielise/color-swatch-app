7c919fdea34ee3ab4510e526128787ed28fff26a
Lillian Gales <lilliangales@ymail.com>
2020-04-04 21:19:21 -0700

    started on color family mobile view

--- src/components/navigation/SideNav.js
+++ src/components/navigation/SideNav.js
@@ -3,34 +3,113 @@ import { Link } from 'react-router-dom'
 import './SideNav.css'
 
 const SideNav = props => {
-
   // TODO: https://www.cs.rit.edu/~ncs/color/t_convert.html
   // TODO: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
   return (
-    <div className="side-nav">
-      <ul id="side-nav-list">
-        <Link
-          to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          id="random-color-button"
-        >
-          Random Color
-        </Link>
-        <Link
-          to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          id="random-color-button"
-        >
-          Redd
-        </Link>
-        <li>Red</li>
-        <li>Orange</li>
-        <li>Yellow</li>
-        <li>Green</li>
-        <li>Blue</li>
-        <li>Purple</li>
-        <li>Brown</li>
-        <li>Gray</li>
-      </ul>
-    </div>
+    <>
+      <div className="side-nav">
+        <ul className="side-nav-list">
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+            id="random-color-button"
+          >
+            Random Color
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Red
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Orange
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Yellow
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Green
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Blue
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Purple
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Brown
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Gray
+          </Link>
+        </ul>
+      </div>
+
+      <div className="side-nav-dropdown">
+        Filter
+        <ul className="side-nav-content-dropdown">
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+            id="random-color-button"
+          >
+            Random Color
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Red
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Orange
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Yellow
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Green
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Blue
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Purple
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Brown
+          </Link>
+          <Link
+            to={{ pathname: '/details', state: { comingFromSelected: false } }}
+          >
+            Gray
+          </Link>
+        </ul>
+      </div>
+    </>
   )
 }
 
