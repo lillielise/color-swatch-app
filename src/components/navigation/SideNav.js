f839b817762e7731e6eb4536cfe53704d5c6a075
Lillian Gales <lilliangales@ymail.com>
2020-04-04 22:51:12 -0700

    tweaked styling, added mobile view, and took out color families

--- src/components/navigation/SideNav.js
+++ src/components/navigation/SideNav.js
@@ -15,97 +15,16 @@ const SideNav = props => {
           >
             Random Color
           </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Red
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Orange
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Yellow
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Green
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Blue
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Purple
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Brown
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Gray
-          </Link>
         </ul>
       </div>
 
-      <div className="side-nav-dropdown">
-        Filter
-        <ul className="side-nav-content-dropdown">
+      <div className="side-nav-mobile">
+        <ul className="side-nav-list">
           <Link
             to={{ pathname: '/details', state: { comingFromSelected: false } }}
             id="random-color-button"
           >
             Random Color
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Red
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Orange
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Yellow
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Green
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Blue
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Purple
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Brown
-          </Link>
-          <Link
-            to={{ pathname: '/details', state: { comingFromSelected: false } }}
-          >
-            Gray
           </Link>
         </ul>
       </div>
