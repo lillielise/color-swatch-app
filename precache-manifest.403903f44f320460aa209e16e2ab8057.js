7c1d1fc208c2e0a8e2dc735a5d8a9fce4f08112a
Lillian Gales <lilliangales@ymail.com>
2020-04-29 11:40:53 -0700

    Updates

---
+++ precache-manifest.403903f44f320460aa209e16e2ab8057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845d6aeab8f1d8e8336b62990562ac3b",
+    "url": "/color-swatch-app/index.html"
+  },
+  {
+    "revision": "296260c74b920fd7b614",
+    "url": "/color-swatch-app/static/css/main.efe16471.chunk.css"
+  },
+  {
+    "revision": "df38bdb04f22fa0240e1",
+    "url": "/color-swatch-app/static/js/2.eea7c23b.chunk.js"
+  },
+  {
+    "revision": "296260c74b920fd7b614",
+    "url": "/color-swatch-app/static/js/main.fc2de095.chunk.js"
+  },
+  {
+    "revision": "3b5e96cd69a4dde4265d",
+    "url": "/color-swatch-app/static/js/runtime-main.470a4222.js"
+  }
+]);
